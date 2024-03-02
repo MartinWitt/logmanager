b2b37e5a81d6ae412e4539946186a27ac5b7ca8f
Martin Wittlinger <wittlinger.martin@gmail.com>
2024-03-03 00:39:31 +0100

    Update base path in DockerServicesControllerApi
    The base path in DockerServicesControllerApi configuration has been updated. This change was made to clean the hardcoded base path. Now, the application will use the relative base path by default.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ import { DockerServiceTable } from "./components/DockerServiceTable.tsx";
 
 const dockerServicesApi = new DockerServicesControllerApi(
   new Configuration({
-    basePath: "127.0.0.1:8080",
+    basePath: "",
   }),
 );
 
